abfc5b999f997b4923da5addd9d96a8b3dc53f81
Naima Hassan <naima.hassan434@gmail.com>
2022-09-15 22:42:23 +0100

    Implementing React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage></HomePage>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
